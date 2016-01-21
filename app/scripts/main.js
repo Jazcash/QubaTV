879043eeeca992fbc7231919d0f0086bfc479b73
jazcash <jaspercashmore@gmail.com>
2016-01-21 22:08:41 +0000

    Sliding carousel!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+$(window).on('beforeunload', function(){
+	$("body").scrollLeft(0);
+});
+
+$(document).ready(function(){
+	var currentPage = 0;
+	var pageLoopId = setInterval(function(){
+		if ($(".pages").children().eq(currentPage + 1).length > 0){
+			$("body").animate({scrollLeft: $(".pages").children().eq(currentPage + 1).position().left}, 800);
+			currentPage++;
+		} else {
+			$("body").animate({scrollLeft: $("page-twitter").position().left}, 800);
+			currentPage = 0;
+		}
+	}, 5000);
+});
