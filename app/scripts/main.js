4fd3a793045b9e45047099e840ca7eba45fb161b
jazcash <jaspercashmore@gmail.com>
2016-01-21 22:41:51 +0000

    Stuff!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,13 @@
-$(window).on('beforeunload', function(){
-	$("body").scrollLeft(0);
-});
-
 $(document).ready(function(){
-	var currentPage = 0;
-	var pageLoopId = setInterval(function(){
-		if ($(".pages").children().eq(currentPage + 1).length > 0){
-			$("body").animate({scrollLeft: $(".pages").children().eq(currentPage + 1).position().left}, 800);
-			currentPage++;
-		} else {
-			$("body").animate({scrollLeft: $("page-twitter").position().left}, 800);
-			currentPage = 0;
-		}
-	}, 5000);
+	//$("body").animate({scrollLeft: $(".pages").children().eq(0).position().left}, 800);
+	//var currentPage = 0;
+	// var pageLoopId = setInterval(function(){
+	// 	if ($(".pages").children().eq(currentPage + 1).length > 0){
+	// 		$("body").animate({scrollLeft: $(".pages").children().eq(currentPage + 1).position().left}, 800);
+	// 		currentPage++;
+	// 	} else {
+	//		currentPage = 0;
+	// 		$("body").animate({scrollLeft: $(".pages").children().eq(currentPage).position().left}, 800);
+	// 	}
+	// }, 5000);
 });
